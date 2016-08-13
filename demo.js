6576d1a12bbffbf40de701f7913cbb874fadf0dd
ccali14 <calicagatay@icloud.com>
2016-08-13 07:51:57 -0700

    Init

--- demo.js
+++ demo.js
@@ -1,5 +1,20 @@
 var volumes = require('./lib/index');
 
-volumes.set(90).then(function(response) {
+volumes.set(0).then(function(response) {
+  console.log(response);
+});
+
+volumes.increase(5).then(function(response) {
+  console.log(response);
+});
+
+volumes.increase(5).then(function(response) {
+  console.log(response);
+});
+volumes.increase(50).then(function(response) {
+  console.log(response);
+});
+
+volumes.decrease(60).then(function(response) {
   console.log(response);
 });
